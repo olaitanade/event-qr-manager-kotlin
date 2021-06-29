e1de5dac3a3ed9ab506f257baa5e8cb0d313a3a1
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2021-06-29 16:41:21 +0100

    added ticketdao

--- data/src/main/java/com/adetayoolaitan/data/room/dao/TicketDao.kt
+++ data/src/main/java/com/adetayoolaitan/data/room/dao/TicketDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.adetayoolaitan.data.room.entity.Event
 import com.adetayoolaitan.data.room.entity.Ticket
 import kotlinx.coroutines.flow.Flow
 
